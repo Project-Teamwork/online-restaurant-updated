25b6a1b5db044cd3197a02bdd819fc11d25ea1f9
Anita Angelidou <anitawebdesigns@gmail.com>
2023-01-29 19:53:11 +0200

    created page to create a new Product

--- app/restaurant-admin/src/Screens/Product.js
+++ app/restaurant-admin/src/Screens/Product.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import { useState } from 'react';
-import { Modal } from 'react-bootstrap';
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom/dist";
+import SideNavbar from "../Components/Sidebar";
+import TopNavbar from "../Components/Navbar";
+import ContainerFluid from "../Components/Container";
+import Loader from "../Components/Loader";
 
 function Product({ pizza }) {
-  const [variant, setVariant] = useState('small');
-  const [show, setShow] = useState(false);
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  // const [variant, setVariant] = useState('small');
+  // const [show, setShow] = useState(false);
+
+  // const handleClose = () => setShow(false);
+  // const handleShow = () => setShow(true);
+
+  const showProducts = async () => {
+    setLoading(true);
+    await navigate("/home");
+  };
 
   return (
-    <div className="pizzaMain shadow-lg p-3 mb-5 bg-white rounded">
-      {/* <div onClick={handleShow}>
+    <div className="d-flex">
+      <SideNavbar />
+      <ContainerFluid>
+        <TopNavbar
+          href="/home"
+          children="Products"
+          onClick={showProducts}
+          action="Back to Products"
+        />
+        <div className="pizzaMain shadow-lg p-3 mb-5 bg-white rounded">
+          {/* <div onClick={handleShow}>
         <h1>{pizza.name}</h1>
         <img src={pizza.image} className="img-fluid" alt="pizza" />
       </div>
@@ -85,6 +106,8 @@ function Product({ pizza }) {
           </button>
         </Modal.Footer>
       </Modal> */}
+        </div>
+      </ContainerFluid>
     </div>
   );
 }
